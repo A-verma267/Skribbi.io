bc5df023f40619b6c5858ebbdd98b97a03f35fc4
Anshika Verma <anshikaver2005@gmail.com>
2026-08-08 14:22:38 +0530

    readytodeploy

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,20 @@ import { Server } from "socket.io";
 import cors from "cors";
 import { getRandomWords } from "./utils/getRandomWords.js";
 
+require("dotenv").config();
+
+
 const rooms = {};
 
 const app = express();
 app.use(cors());
 
+const port = process.env.PORT || 3001;
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST"],
     },
 });
